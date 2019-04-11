b74c08c7ec6a61925dc8afa50e737e3d4b64d64a
nian0114 <268078545@qq.com>
2019-04-11 14:07:01 +0800

    get free torrent list

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import requests
 from config import *
+from bs4 import BeautifulSoup
 
 def login():
     with requests.Session() as s:
@@ -8,7 +9,19 @@ def login():
         print(r.headers)
         return s
 
+def getFreeTorrent(t,class_name):
+    soup = BeautifulSoup(t.text, 'html.parser')
+    soup_result = soup.find_all(class_=class_name)
+    
+    return soup_result
+
 s=login()
 t = s.get(ourBitsUrl+"torrents.php")
-print(t.text)
-print(t.headers)
+
+for tr in getFreeTorrent(t,"sticky_top"):
+    if 'Free' in str(tr.contents[3]):
+        print(tr)
+
+for tr in getFreeTorrent(t,"sticky_normal"):
+    if 'Free' in str(tr.contents[3]):
+        print(tr)
